d12f29a6140a0cf3989c9cd8fd8b0b6027e6482b
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-13 19:00:24 -1000

    add isEven function

--- functions.js
+++ functions.js
@@ -169,6 +169,9 @@ function maximum(a,b) {
  * @param {number} n
  * @return {boolean} the number is even
  */
+function isEven(n) {
+  return (n % 2) == 0;
+}
 
 /**
  * Returns true if `n` is odd.
