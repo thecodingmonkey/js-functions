2981eabcd97b3a8127a6272cd1045ef1e11b2368
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-13 19:35:56 -1000

    add combine function

--- functions.js
+++ functions.js
@@ -231,6 +231,10 @@ function letterGrade(score, total) {
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
+function combine(word1, word2) {
+  return word1 + " " + word2;
+}
+
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
